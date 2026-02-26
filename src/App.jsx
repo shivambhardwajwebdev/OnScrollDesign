ca32a22ea33e6ca2a824b84b5500224099aa317c
shivambhardwajwebdev <shiva.bhardwaj.mks@gmail.com>
2026-02-26 21:44:56 +0530

    set up foundation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import "@fontsource/open-sans"; 
+
+function App() {
+  return(
+    <>
+    <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
